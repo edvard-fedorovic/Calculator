cf3b6b50c50634f267f1a7af8c1de43336c043f9
edvard-fedorovic <78847715+edvard-fedorovic@users.noreply.github.com>
2021-02-17 15:31:18 +0200

    Add info button

--- Project2/MyForm.h
+++ Project2/MyForm.h
@@ -45,6 +45,7 @@ namespace Project2 {
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Label^ label4;
 	private: System::Windows::Forms::ComboBox^ comboBox1;
+	private: System::Windows::Forms::Button^ button3;
 
 	protected:
 
@@ -71,6 +72,7 @@ namespace Project2 {
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// textBox1
@@ -177,11 +179,22 @@ namespace Project2 {
 			this->comboBox1->Text = L"Сложение";
 			this->comboBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &MyForm::comboBox1_SelectedIndexChanged);
 			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(12, 372);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(311, 23);
+			this->button3->TabIndex = 10;
+			this->button3->Text = L"Info";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(340, 399);
+			this->Controls->Add(this->button3);
 			this->Controls->Add(this->comboBox1);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->button2);
@@ -259,5 +272,8 @@ namespace Project2 {
 		textBox1->Text = L"";
 		label4->Text = L"";
 	}
+private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+	MessageBox::Show("This program was created by Edvard Fedorovich!!","Creator");
+}
 };
 }
